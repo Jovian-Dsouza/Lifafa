fba53d2107eefdaf3adf8f70d4ffa9beaeb924e3
Jovian Dsouza <dsouzajovian123@gmail.com>
2024-05-05 18:07:59 +0530

    Add image assests

--- app/assets/assets.ts
+++ app/assets/assets.ts
@@ -1,4 +1,10 @@
 export const images = {
-    lifafa: require('./images/lifafa.png'),
-    okto: require('./images/okto.png')
-}
+  lifafa: require("./images/lifafa.png"),
+  okto: require("./images/okto.png"),
+  tokens: {
+    bonk: require("./images/tokens/bonk.jpeg"),
+    sol: require("./images/tokens/sol.jpeg"),
+    usdc: require("./images/tokens/usdc.jpeg"),
+    wif: require("./images/tokens/wif.jpeg"),
+  },
+};
